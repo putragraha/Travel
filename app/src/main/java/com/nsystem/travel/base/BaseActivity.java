28a8a6f182e2fb29d95d5f33acb87121372ee46d
Putra Nugraha <putra.nugraha@dana.id>
2020-08-03 19:33:59 +0700

    Remove Feature Flags

--- app/src/main/java/com/nsystem/travel/base/BaseActivity.java
+++ app/src/main/java/com/nsystem/travel/base/BaseActivity.java
@@ -1,14 +1,10 @@
 package com.nsystem.travel.base;
 
-import com.nsystem.travel.R;
-import com.nsystem.travel.TravelApplication;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -20,34 +16,12 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initTravelable();
+        setContentView(getView());
+        setup();
     }
 
     protected <T> void startActivity(Class<T> clazz) {
         Intent intent = new Intent(this, clazz);
         startActivity(intent);
-    }
-
-    private void initTravelable() {
-        if (isTravelable()) {
-            setContentView(getView());
-            setup();
-        } else {
-            showRoxMessage();
-        }
-    }
-
-    private boolean isTravelable() {
-        return ((TravelApplication) getApplication()).getFlags().getTravelable().isEnabled();
-    }
-
-    private void showRoxMessage() {
-        new AlertDialog.Builder(this)
-            .setMessage(R.string.rox_message)
-            .setIcon(R.mipmap.ic_launcher)
-            .setCancelable(false)
-            .setPositiveButton(android.R.string.yes, (dialog, which) -> dialog.cancel())
-            .create()
-            .show();
     }
 }
